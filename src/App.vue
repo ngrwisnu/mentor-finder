57e0340c506043159fec957982ccb9751cc3915a
ngrwisnu <adiprayogongrwisnu@gmail.com>
2024-06-03 07:58:25 +0700

    init setup

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { RouterView } from 'vue-router'
+
+export default {
+  components: {
+    RouterView
+  }
+}
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Poppins', sans-serif;
+  font-weight: 200;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script lang="ts"></script>
+
+<template>
+  <main>
+    <h1>Hello Vue 3 - Mentor Finder App</h1>
+  </main>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 400;
+}
+</style>
